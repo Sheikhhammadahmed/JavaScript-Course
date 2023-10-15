e695383d84328d7476b62d30ad8787c975ba5c61
Hammad Scheikh <digitalaccess261@gmail.com>
2023-10-15 20:14:13 +0500

    class01

---
+++ JavaScript Class 01/app.js
@@ -0,0 +1,35 @@
+var name1 = "Ali"
+var age1 = 24
+var roll1 = 43
+
+var name2 = "Khan"
+var age2 = 25
+var roll2 = 44
+
+var name3 = "Sultan"
+var age3 = 25
+var roll3 = 45
+
+
+document.write(
+    "<table border='1px '>" +
+    '<tr>' +
+    '<th>' + "Name" + '</th>'
+    + '<th>' + "Age" + '</th>'
+    + '<th>' + "Roll no" + '</th>'
+    + '</tr>' +
+    "<td>" + name1 + "</td>" +
+    "<td>" + age1 + "</td>" +
+    "<td>" + roll1 + "</td>" +
+    '<tr>' +
+    "<td>" + name2 + "</td>" +
+    "<td>" + age2 + "</td>" +
+    "<td>" + roll2 + "</td>" +
+    '</tr>' +
+    '<tr>' +
+    "<td>" + name3 + "</td>" +
+    "<td>" + age3 + "</td>" +
+    "<td>" + roll3 + "</td>" +
+    '</tr>' +
+    '</table>'
+)
